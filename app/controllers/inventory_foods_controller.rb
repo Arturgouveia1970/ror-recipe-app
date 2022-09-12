f7accd4f35337a56d79698522e90ebe9e83356a6
Felix Odete <felix.odette@gmail.com>
2022-09-12 22:58:31 +0300

    Linters fix

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -20,8 +20,7 @@ class InventoryFoodsController < ApplicationController
     food_list = params[:inventory_food][:food_list]
     food_list = food_list.drop(1)
     food_list.each do |food|
-      quantity = Food.find(food.to_i).quantity
-      new_inventory_food = InventoryFood.new(food_id: food.to_i, quantity:, inventory_id: params[:id])
+      Food.find(food.to_i).quantity
     end
     redirect_to inventory_path(params[:id]), flash: { success: 'Inventory has been created succesfully!' }
   end
