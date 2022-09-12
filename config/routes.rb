37d1c4828e7b3178a9460e23cf8e1171400d76a0
Felix Odete <felix.odette@gmail.com>
2022-09-12 22:31:33 +0300

    Controllers: Inventory foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  delete 'inventory_foods/:id/destroy/:food_id', to: 'inventory_foods#destroy'
+  get 'inventory_foods/:id/new', to: 'inventory_foods#new'
+  post 'inventory_foods/:id/create', to: 'inventory_foods#create'
   get 'user/index'
   get 'user/show'
   resources :inventories, only: %i[index show new create destroy]
