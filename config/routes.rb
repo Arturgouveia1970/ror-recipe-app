d1ba59c546c48ddca42ee80575b3432c8aadf575
Felix Odete <felix.odette@gmail.com>
2022-09-12 21:51:29 +0300

    Update: routes, validation and relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get 'user/index'
   get 'user/show'
-  get '/inventories', to: 'inventory#index'
-  get 'inventories/:id', to: 'inventory#show', as: 'inventory'
+  resources :inventories, only: %i[index show new create destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "user#index"
+  root 'user#index'
 end
