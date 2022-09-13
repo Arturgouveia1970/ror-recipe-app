9549375aeaed3f6a606d8e85a230410332f2c421
principles31 <principles31@gmail.com>
2022-09-13 11:13:42 +0000

    Fix rubocop error

--- app/controllers/foodlist_controller.rb
+++ app/controllers/foodlist_controller.rb
@@ -4,17 +4,17 @@ class FoodsController < ApplicationController
   # GET /foods or /foods.json
   def index
     @foods = Food.all
-    end
+  end
 
   # GET /foods/1 or /foods/1.json
   def show
     @foods = Food.find(params[:id])
-    end
+  end
 
   # GET /foods/new
   def new
     @food = Food.new
-    end
+  end
 
   # GET /foods/1/edit
   def edit; end
@@ -23,7 +23,7 @@ class FoodsController < ApplicationController
   def create
     @food = Food.new(food_params)
 
-      respond_to do |format|
+    respond_to do |format|
         if @food.save
           format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
           format.json { render :show, status: :created, location: @food }
@@ -32,11 +32,11 @@ class FoodsController < ApplicationController
           format.json { render json: @food.errors, status: :unprocessable_entity }
         end
       end
-    end
-  
+  end
+
   # PATCH/PUT /foods/1 or /foods/1.json
   def update
-      respond_to do |format|
+    respond_to do |format|
         if @food.update(food_params)
           format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
           format.json { render :show, status: :ok, location: @food }
@@ -45,27 +45,27 @@ class FoodsController < ApplicationController
           format.json { render json: @food.errors, status: :unprocessable_entity }
         end
       end
-    end
+  end
 
   # DELETE /foods/1 or /foods/1.json
   def destroy
     @food.destroy
-  
-      respond_to do |format|
+
+    respond_to do |format|
         format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
         format.json { head :no_content }
       end
-    end
+  end
 
     private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_food
     @food = Food.find(params[:id])
-    end
+  end
 
   # Only allow a list of trusted parameters through.
   def food_params
-      params.fetch(:food, {})
-    end
+    params.fetch(:food, {})
   end
+end
