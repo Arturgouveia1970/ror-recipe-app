875cfe685188337c1c2486d608458dbe0bdcab51
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-13 18:42:41 +0100

    organized tables

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy, class_name: 'RecipeFood'
+  has_many :foods, through: :recipe_foods
 end
--- db/migrate/20220912185311_add_description_to_inventories.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDescriptionToInventories < ActiveRecord::Migration[7.0]
-  def change
-    add_column :inventories, :description, :text
-  end
-end
