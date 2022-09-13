f6cb9f0460f5790aa81c59454ace80257b1ff245
Felix Odete <felix.odette@gmail.com>
2022-09-13 08:48:03 +0300

    Update: Cancan ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,12 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    #   return unless user.present?
+    return unless user.present?
+
+    can :manage, Recipe, user_id: user.id
     #   can :read, :all
     #   return unless user.admin?
     #   can :manage, :all
