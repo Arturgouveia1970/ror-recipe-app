3a75e6be59f3b6a5d96b66543a3c1f477c4d0e06
principles31 <principles31@gmail.com>
2022-09-13 11:07:28 +0000

    Fix rubocop error

--- app/controllers/foodlist_controller.rb
+++ app/controllers/foodlist_controller.rb
@@ -1,29 +1,29 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
 
-    # GET /foods or /foods.json
+  # GET /foods or /foods.json
   def index
-      @foods = Food.all
+    @foods = Food.all
     end
 
-    # GET /foods/1 or /foods/1.json
+  # GET /foods/1 or /foods/1.json
   def show
-      @foods = Food.find(params[:id])
+    @foods = Food.find(params[:id])
     end
 
-    # GET /foods/new
+  # GET /foods/new
   def new
-      @food = Food.new
+    @food = Food.new
     end
 
-    # GET /foods/1/edit
+  # GET /foods/1/edit
   def edit; end
 
-    # POST /foods or /foods.json
+  # POST /foods or /foods.json
   def create
-      @food = Food.new(food_params)
+    @food = Food.new(food_params)
 
-    respond_to do |format|
+      respond_to do |format|
         if @food.save
           format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
           format.json { render :show, status: :created, location: @food }
@@ -33,8 +33,8 @@ class FoodsController < ApplicationController
         end
       end
     end
-
-    # PATCH/PUT /foods/1 or /foods/1.json
+  
+  # PATCH/PUT /foods/1 or /foods/1.json
   def update
       respond_to do |format|
         if @food.update(food_params)
@@ -47,10 +47,10 @@ class FoodsController < ApplicationController
       end
     end
 
-    # DELETE /foods/1 or /foods/1.json
+  # DELETE /foods/1 or /foods/1.json
   def destroy
-      @food.destroy
-
+    @food.destroy
+  
       respond_to do |format|
         format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
         format.json { head :no_content }
@@ -58,14 +58,14 @@ class FoodsController < ApplicationController
     end
 
     private
-  
-    # Use callbacks to share common setup or constraints between actions.
+
+  # Use callbacks to share common setup or constraints between actions.
   def set_food
-      @food = Food.find(params[:id])
+    @food = Food.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+  # Only allow a list of trusted parameters through.
   def food_params
       params.fetch(:food, {})
     end
-end
+  end
