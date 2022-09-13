c0edb1f9c0d2144dd14c31638b5d6e61fc80db27
principles31 <principles31@gmail.com>
2022-09-13 14:51:29 +0000

    fix error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,6 @@ class RecipesController < ApplicationController
   # before_action :set_recipe, only: %i[ show edit update destroy ]
 
   # GET /recipes or /recipes.json
-  def index
 
-  end
+end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,2 @@
 class Food < ApplicationRecord
-
 end
