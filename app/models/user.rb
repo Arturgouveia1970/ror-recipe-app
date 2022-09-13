c85964c1da4c8de90e3757a58ac338d66d78a16e
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-13 14:23:52 +0100

    created recipes, controllers and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :recipe
+  # has_many :recipes, foreign_key: user_id, class_name: 'Recipe'
   has_many :inventory
 end
---
+++ db/migrate/20220913102440_change_user_id_to_be_bigint_in_recipes.rb
@@ -0,0 +1,5 @@
+class ChangeUserIdToBeBigintInRecipes < ActiveRecord::Migration[7.0]
+  def change
+    change_column :recipes, user_id, :bigint
+  end
+end
