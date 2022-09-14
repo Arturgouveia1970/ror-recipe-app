95e095b3361d965474ed459e78daabbc9fd511bb
principles31 <principles31@gmail.com>
2022-09-14 15:05:48 +0000

    Add intergration

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.first
+  subject { Inventory.new(name: 'test inventory', user:) }
+  before { subject.save }
+
+  it 'name should not be blank' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
